c70842780a63b2e6fa6ef94d0548421bc45dc10a
mejowett <78455126+mejowett@users.noreply.github.com>
2021-12-27 15:09:00 -0500

    textarea

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
+
+import { useState } from 'react';
 import ReactDom from 'react-dom';
+
 const App = () => {
-  return <h1>hi</h1>;
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 ReactDom.render(<App />, document.querySelector('#root'));
