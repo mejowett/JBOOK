7cbade6e7f04172d55dd6a808327808413f93dd1
mejowett <78455126+mejowett@users.noreply.github.com>
2021-12-28 14:03:51 -0500

    tiny-test-pkg

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,5 @@
 import * as esbuild from 'esbuild-wasm';
- 
+
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
@@ -8,22 +8,17 @@ export const unpkgPathPlugin = () => {
         console.log('onResole', args);
         return { path: args.path, namespace: 'a' };
       });
- 
+
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
- 
+
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
             contents: `
-              import message from './message';
+              import message from 'tiny-test-pkg';
               console.log(message);
             `,
-          };
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
           };
         }
       });
